5298125bad451a97eaf25dbea022d0e71c694b0b
S.Park <seongmin.a.park@gmail.com>
2019-11-30 14:10:21 -0800

    Add files via upload

---
+++ SocialInfo_Bayesian.m
@@ -0,0 +1,46 @@
+function [GoF, LL] = SocialInfo_Bayesian(params,J1,C,Js,Ch,Gr,nTrial)
+    %global LL
+    for tr=1:nTrial
+        
+        % Judgment range 0 to 30 years
+        J_rng = [0:1:30];
+        n=max(J_rng);
+        
+        %Prior
+        J1_mu = J1(tr);
+        J1_sigma = 1/(params(1)+params(2).*C(tr));
+        
+        %Liklihood 
+        Joth_mu = Js(tr);
+        Joth_tau = 1/(params(3)+params(4).*Gr(tr));
+        
+        % Posterior
+        J2_mu =(Joth_tau*Joth_mu+J1_sigma*J1_mu)/(J1_sigma+Joth_tau);
+        J2_sigma = (J1_sigma*Joth_tau)/(J1_sigma+Joth_tau);        
+        
+        S0 = normpdf(J_rng,J1_mu,J1_sigma); % prior belief self
+        S1 = normpdf(J_rng,J2_mu,J2_sigma); % posterior belief self
+        
+        S0=S0/sum(S0);
+        S1=S1/sum(S1);
+        
+        nonzeromin = 0.1e-15; % Considering that it is not possible to compute DKL for log(0) = -Inf.
+        S0(find(S0<nonzeromin))=nonzeromin;
+        S1(find(S1<nonzeromin))=nonzeromin;
+        KL(tr) = -dot(S0,log2(S1)-log2(S0));
+
+    end
+    
+    GoF=-corr(KL',Ch);
+    outputeta=glmfit(KL,Ch);
+    eta = outputeta(2);
+    ChHat= outputeta(2)* KL'+outputeta(1);
+    RS = Ch-ChHat;
+    epsilon = var(RS);
+
+    for tr=1:nTrial
+        L(tr) =-(log(2*pi*epsilon)+1/epsilon*(Ch(tr)-ChHat(tr))^2)/2;
+    end
+    LL=sum(L);
+
+end
---
+++ SocialInfo_ParamEstimate.m
@@ -0,0 +1,74 @@
+
+close all;
+clear all;
+clc;
+
+%% Read data
+load('bhv_data.mat'); % sample data
+
+global J1 C Js Ch Gr nTrial %LL
+J1 = bhv_data(:,1);                 %J1, initial judgment
+J2 = bhv_data(:,2);                 %J2, second judgment
+Js=bhv_data(:,3);                    %Js, social judgment
+C = bhv_data(:,4);                   %C, confidence ratings, normalized to [0-1]
+Dir=Js-J1;
+Ch=(J2-J1).*Dir./abs(Dir);      %Changes in judgments
+Gr=bhv_data(:,5);                     %Group size [0;1]=[large;small]
+nTrial=length(bhv_data);        % Number of trials
+
+%% parameter estimates
+
+% Find the parameters that give the best fit from m x n trials of parameter estimation. 
+m_sample = 100; % sample size of parameter estimates 
+n_sample = 5; % trial of parameter optimization
+
+tic
+for j=1:n_sample       
+    for i = 1:m_sample
+        clear b allloglike
+
+         if j==1
+            J1_beta0(j,i) = rand(1)*10;
+            J2_omega0(j,i) = rand(1)*10;
+            Js_beta0(j,i) = rand(1)*10;
+            Js_omega0(j,i) = rand(1)*10;
+         else
+            % Use the best parameters from the previous batch as the initial values of params for new batch 
+            J1_beta0(j,i) = prams_otim(2)+rand(1);
+            J2_omega0(j,i) = prams_otim(3)+rand(1);
+            Js_beta0(j,i) =  prams_otim(4)+rand(1);
+            Js_omega0(j,i) =  prams_otim(5)+rand(1);
+            clear prams_otim
+        end
+
+        inx0 = [J1_beta0(j,i) J2_omega0(j,i) Js_beta0(j,i) Js_omega0(j,i)];
+        result.inx0{i} = inx0;
+
+        options = optimset('Display','off','MaxIter',100000,'TolFun',1e-10,'TolX',1e-10,...
+            'DiffMaxChange',1e-2,'DiffMinChange',1e-4,'MaxFunEvals',100000,'LargeScale','off');
+        warning off; 
+        lb = [0, 0, 0, 0];   %lower bounds on parameters
+        ub = [Inf, Inf, Inf, Inf]; %upper bounds on parameters
+
+        [params, GoF] = fmincon(@SocialInfo_Bayesian, inx0, [],[],[],[],lb,ub,[],options,J1,C,Js,Ch,Gr,nTrial); 
+        [GoF] = SocialInfo_Bayesian(params, J1,C,Js,Ch,Gr,nTrial);
+
+        J1_beta(i)= params(1);
+        J1_omega(i)= params(2);
+        Js_beta(i) = params(3);
+        Js_omega(i) = params(4);
+        param_space(i,:)=[GoF, params];
+        prams_otim=param_space(find(param_space==min(param_space(:,1))),:);
+
+    end
+    best_samples(j,:)=prams_otim(1,:);
+end
+
+ParamHat=mean(best_samples(find(best_samples(:,1)==min(best_samples(:,1))),2:end),1);
+[GoF, LL] = SocialInfo_Bayesian(ParamHat, J1,C,Js,Ch,Gr,nTrial);
+
+BIC = -2*LL + length(ParamHat);
+fprintf ('Loglikelihood: %f, BIC: %f \n ', LL, BIC);
+ParamHat
+
+toc
